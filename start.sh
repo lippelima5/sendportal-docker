bd084244bae1b16e01e5d09cd9eefd23269bf8e6
Felippe <oliveiralippe@gmail.com>
2022-12-30 00:45:53 -0300

    1.0

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source .env
+
+# Cria uma iamgem docker do sendportal usando Dockerfile
+docker build -t ${CONTAINER_NAME} docker
+
+# Cria um container redis
+docker run -d -p 6379:6379 --name ${CONTAINER_NAME}_redis --rm redis
+
+# Run app
+# docker run --rm -d -p 8181:8181 --name ${CONTAINER_NAME} --link ${CONTAINER_NAME}_redis:redis -v /app ${CONTAINER_NAME}
+docker run --rm -d -p 8181:8181 --name ${CONTAINER_NAME} -v /app ${CONTAINER_NAME}
+
+# Run sendportal setup
+docker exec -it ${CONTAINER_NAME} /app/artisan key:generate && service cron start && /app/artisan sp:install 
+
