784b1feff0ac20d307799f0908a58d65e37209d9
Felippe <oliveiralippe@gmail.com>
2023-01-04 20:15:03 -0300

    remove -rm flag

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ source .env
 docker build -t ${CONTAINER_NAME} docker
 
 # Cria um container redis
-docker run -d -p 6379:6379 --name ${CONTAINER_NAME}_redis --rm redis
+docker run -d -p 6379:6379 --name ${CONTAINER_NAME}_redis --restart unless-stopped redis
 
 # Run app
 # docker run --rm -d -p 8181:8181 --name ${CONTAINER_NAME} --link ${CONTAINER_NAME}_redis:redis -v /app ${CONTAINER_NAME}
-docker run --rm -d -p 8181:8181 --name ${CONTAINER_NAME} -v /app ${CONTAINER_NAME}
+docker run -d -p 8181:8181 --name ${CONTAINER_NAME} -v /app --restart unless-stopped ${CONTAINER_NAME}
 
 # Run sendportal setup
 docker exec -it ${CONTAINER_NAME} /app/artisan key:generate  
